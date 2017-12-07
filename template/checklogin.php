6f9c2733c9d0b1bb490c9c8df314057923785ed9
yilinmiao <yi.matheson@gmail.com>
2017-12-06 19:50:13 -0800

    fixed redirect index page issue

--- template/checklogin.php
+++ template/checklogin.php
@@ -1,17 +1,20 @@
-
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include('../content/users_dbh.php');
 if (isset($_POST['submit'])
     && !empty($_POST['uid'])
     && !empty($_POST['pwd'])) {
     $uid = $_POST['uid'];
     $pwd = $_POST['pwd'];
-    echo 'SELECT * FROM users WHERE uid = "'.$uid.'" and user_password = "'.$pwd.'"';
+    // echo 'SELECT * FROM users WHERE uid = "'.$uid.'" and user_password = "'.$pwd.'"';
     $result = mysqli_query($conn, 'SELECT * FROM users WHERE uid = "'.$uid.'" and user_password = "'.$pwd.'"');
     $resultCheck = mysqli_num_rows($result);
     if ($resultCheck > 0) {
         session_start();
         $_SESSION['username'] = $uid;
+        // echo $_SESSION['username'];
         header("Location: ../index.php");
     } else {
         //echo 'fail';
